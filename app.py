4d1b2e63e183a2432035b8006bdaeb184ca10a4c
Vitor Hashimoto <101890538+VitorHashimoto21@users.noreply.github.com>
2025-11-11 22:40:49 +0000

    Criação de tela HTML para hall do dash

--- app.py
+++ app.py
@@ -1,27 +1,28 @@
+from flask import Flask, render_template
 from dash import Dash
+from modules.layout import criar_layout
+from modules.callbacks import registrar_callbacks
 
-from layout import criar_layout
-from callbacks import registrar_callbacks
-from dash import dcc, html, Input, Output
-from track_map import gerar_mapa_comparativo
-from tabela_pilotos import gerar_tabela_pilotos_sessao
+# --- FLASK SERVER ---
+server = Flask(__name__)
 
+# Página inicial HTML (a tela estilizada)
+@server.route('/')
+def home():
+    return render_template('main.html')
 
+# --- DASH APP ---
+app = Dash(
+    __name__,
+    server=server,
+    url_base_pathname='/dash/',   # necessário terminar com '/'
+    suppress_callback_exceptions=True,
+    title="LapTrack F1"
+)
 
-import fastf1
-
-# Ativar cache
-fastf1.Cache.enable_cache('cache') 
-
-# Inicializar app
-app = Dash(__name__, suppress_callback_exceptions=True)
-server = app.server
-
-# Layout
+# Layout e callbacks do seu projeto
 app.layout = criar_layout()
-
-# Callbacks
 registrar_callbacks(app)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=5000, debug=True)
