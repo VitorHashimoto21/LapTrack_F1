0520b19f356a9fac112942c13fe0380eae0f2f88
Vitor Hashimoto <101890538+VitorHashimoto21@users.noreply.github.com>
2025-11-13 02:31:38 +0000

    Atuaização grande envolvendo telas htm sobre, main e contato

--- app.py
+++ app.py
@@ -6,44 +6,30 @@ from modules.callbacks import registrar_callbacks
 # --- FLASK SERVER ---
 server = Flask(__name__)
 
-# Página inicial HTML (a tela estilizada)
+# --- ROTAS HTML ---
 @server.route('/')
 def home():
     return render_template('main.html')
+
+@server.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
+
+@server.route('/equipe')
+def equipe():
+    return render_template('equipe.html')
 
 # --- DASH APP ---
 app = Dash(
     __name__,
     server=server,
-    url_base_pathname='/dash/',   # necessário terminar com '/'
-    suppress_callback_exceptions=True,
-    title="LapTrack F1"
+    url_base_pathname='/dash/',
+    suppress_callback_exceptions=True
 )
 
-# Layout e callbacks do seu projeto
-from layout import criar_layout
-from callbacks import registrar_callbacks
-from modules.layout import criar_layout
-from modules.callbacks import registrar_callbacks
-from dash import dcc, html, Input, Output
-from modules.track_map import gerar_mapa_comparativo
-from modules.tabela_pilotos import gerar_tabela_pilotos_sessao
-import modules.utils 
-
-
-import fastf1
-
-# Ativar cache
-fastf1.Cache.enable_cache('cache') 
-
-# Inicializar app
-app = Dash(__name__, suppress_callback_exceptions=True)
-server = app.server
-
-# Layout
-main
+# Layout e callbacks
 app.layout = criar_layout()
 registrar_callbacks(app)
 
-if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
